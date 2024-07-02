70c4fece43279ceac688b165488343c05e6f5b34
SofiaCM14 <SofiaCM14>
2024-07-02 16:18:59 -0600

    Nombre de pag

--- PaginaInnova/index.php
+++ PaginaInnova/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos.css" />
-    <title>File Upload</title>
+    <title>INTERAI</title>
 </head>
 <body>
     <header>
